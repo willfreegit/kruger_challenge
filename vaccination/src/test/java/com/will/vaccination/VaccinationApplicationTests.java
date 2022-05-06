506d62a2cade29ce9a91e05ca8dcdb6af0256069
will <willmnging@gmail.com>
2022-05-05 20:41:51 -0500

    Generate the spring boot app

---
+++ vaccination/src/test/java/com/will/vaccination/VaccinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.vaccination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
